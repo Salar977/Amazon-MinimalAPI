0f89b5005ea39ae338cc5830ad6147fcb61eef4b
Salarega <salar831@gmail.com>
2023-09-30 21:00:58 +0200

    small changes

--- Amazon/Models/Books.cs
+++ Amazon/Models/Books.cs
@@ -6,12 +6,12 @@ namespace Amazon.Models
 	{
 		public int Id { get; set; }
 		[Required(ErrorMessage = "Title is required.")]
-		public string Title { get; set; } = string.Empty;
+		public string? Title { get; set; }
 		[Required(ErrorMessage = "Author is required.")]
-		public string Author { get; set; } = string.Empty;
+		public string? Author { get; set; }
 		[Range(1000, 2100, ErrorMessage = "Publication year must be between 1000 and 2100.")]
 		public int PublicationYear { get; set; }
-		public string ISBN { get; set; } = string.Empty;
+		public string? ISBN { get; set; }
 		[MinLength(0, ErrorMessage = "Must be atleast 0")]
 		public int InStock { get; set; }
 	}
